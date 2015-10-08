dec9606a312ad370518edf9e685c814112e43b60
MatheusVigaro <matheus@vigaro.tk>
2015-10-08 20:39:03 -0300

    Remove unused code

--- src/main/java/tk/vigaro/helix/listener/ListenerCommandAnime.java
+++ src/main/java/tk/vigaro/helix/listener/ListenerCommandAnime.java
@@ -62,75 +62,9 @@ public class ListenerCommandAnime extends ListenerAdapter {
                         }
                         break;
                     }
-/*                    case "fetch": {
-                        try {
-                            SyndFeed feed = Util.getSyndFeed("http://www.nyaa.se/?page=rss&cats=1_37&term=" + URLEncoder.encode(q, "UTF-8"));
-                            SyndEntry entry = feed.getEntries().get(0);
-                            assert (q.equals(entry.getTitle()) && q.charAt(q.length()-4) == '.');
-                            event.respond("Anime found! Downloading...");
-                            if (entry.getDescription().getValue().contains("GiB")) {
-                                m = "File too large to download! Maximum file size is 999MiB";
-                                break;
-                            }
-                            if ((new File("/")).getUsableSpace() < 5.369e+9){
-                                m = "Not enough free disk space!";
-                                break;
-                            }
-                            if ((new File("animes/" + q)).exists() && !(new File("animes/" + q + ".tr").exists())) {
-                                String l = (new JSONObject(Util.getHTTPResponse("https://api.waa.ai/shorten?url=" + URLEncoder.encode("http://" + Helix.properties.getProperty("domain") + "/?a=" + Base64.encode(q), "UTF-8") + "&key=" + Helix.properties.getProperty("waaai.apikey")))).getJSONObject("data").getString("url");
-                                m = "Download complete! " + q + " Get it at " + l;
-                                break;
-                            }
-                            if (Helix.clients.containsKey(q) && !Helix.clients.get(q).getState().equals(Client.ClientState.DONE)){
-                                m = "Already downloading this anime, try again later";
-                                break;
-                            }
-                            try {
-                                File trfile = new File("animes/" + entry.getTitle() + ".tr");
-                                if (!trfile.exists()) FileUtils.copyURLToFile(new URL(entry.getLink()), trfile, 60000, 60000);
-                                Client c = new Client(Helix.inetAddress, SharedTorrent.fromFile(new File("animes/" + entry.getTitle() + ".tr"), new File("animes")));
-                                c.setMaxDownloadRate(0.0);
-                                c.setMaxUploadRate(50.0);
-                                Helix.clients.put(q, c);
-                                c.download();
-                                stopAfter(q, 1800000);
-                                c.waitForCompletion();
-                                if (c.getState() != Client.ClientState.DONE) throw new IOException();
-                                Helix.clients.remove(q);
-                                FileUtils.deleteQuietly(new File("animes/" + q + ".tr"));
-                                String l = (new JSONObject(Util.getHTTPResponse("https://api.waa.ai/shorten?url=" + URLEncoder.encode("http://" + Helix.properties.getProperty("domain") + "/?a=" + Base64.encode(q), "UTF-8") + "&key=" + Helix.properties.getProperty("waaai.apikey")))).getJSONObject("data").getString("url");
-                                m = "Download completed! " + q + " Get it at " + l;
-                                break;
-                            } catch (IOException e) {
-                                if (Helix.clients.containsKey(q)) Helix.clients.remove(q);
-                                m = "Download failed!";
-                            }
-
-                        } catch (FeedException | IOException | IndexOutOfBoundsException | AssertionError e) {
-                            m = "Anime not found!";
-                        }
-                        break;
-                    }*/
                 }
                 if (m != null) event.respond(m);
             }
-
         }
     }
-
- /*   private void stopAfter(final String key, final long timeout){
-        new Thread() {
-            public void run() {
-                try {
-                    Thread.sleep(timeout);
-                    if (Helix.clients.containsKey(key)) {
-                        Helix.clients.get(key).stop();
-                        Helix.clients.remove(key);
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-    }*/
 }
